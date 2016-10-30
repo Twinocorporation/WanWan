784e6a2dbc5e63a9104138f790d54a90bf4aba80
qiwei <qiweihuang1993@hotmail.fr>
2016-10-30 16:40:33 +0100

    inscription

--- WanWan/www/js/controllers.js
+++ WanWan/www/js/controllers.js
@@ -3,13 +3,28 @@ angular.module('wanwan.controllers', [])
 .controller('DashCtrl', function($scope) {})
 
 .controller('WanwanCtrl', function($scope,$ionicModal,$ionicPopup, $location) {
-    $ionicModal.fromTemplateUrl('templates/login.html', {
-        scope : $scope,
-        animation: 'slide-in-up'
-    }).then(function(modal) {
-        $scope.loginModal= modal; 
-    });
     
+            
+            
+                 $ionicModal.fromTemplateUrl('templates/login.html', {
+                    scope : $scope,
+                    animation: 'slide-in-up'
+                    }).then(function(modal) {
+                    $scope.loginModal= modal; 
+                    });
+                     
+                 
+                 $ionicModal.fromTemplateUrl('templates/inscrire.html', {
+                    scope : $scope,
+                    animation: 'slide-in-up'
+                    }).then(function(modal) {
+                    $scope.inscrireModal= modal; 
+                    });
+                    
+            
+                
+    
+    // sert à afficher la fenêtre d'alerte dans le cas où le mot de passe ou le username ne correspond pas
     $scope.showAlert = function() {
                 var alertPopup = $ionicPopup.alert({
                     title: '登录名或密码不对',
@@ -20,20 +35,54 @@ angular.module('wanwan.controllers', [])
                     
                     $scope.loginModal.hide();
                 });
-            };
+            }
     
+    // fonction qu'on appelle quand on appuit sur le bouton valider
     $scope.loginNew = function (loginData){
-    
-       
-        
-        if (loginData.username == "qiwei" && loginData.password== "huang") {
+       if (loginData.username === "qiwei" && loginData.password=== "huang") {
+            // si login bon, on  ferme la fenêtre de modal et on envoit la page principal
             $scope.loginModal.hide();
             $location.path('/tab/dash');
         }else {
+            //sinon on affiche la fenêtre d'alerte
             $scope.showAlert();
         }
         
     }
+    
+    $scope.showAlertinscrire =function (){
+        var alertPopup = $ionicPopup.alert({
+                    title: '您没有输入全部信息',
+                    template: '请重新输入'
+                });
+                
+                alertPopup.then(function(res){
+                    
+                    $scope.inscrireModal.hide();
+                });
+    }
+    
+    // fonction qui permet de s'inscrire
+    $scope.inscrire = function (inscrireData) {
+        
+        console.log(inscrireData.value1);
+        console.log(inscrireData.value2);
+        console.log(inscrireData.email);
+        
+        if (inscrireData.email == null || inscrireData.password == null || inscrireData.username == null) {
+            
+            $scope.showAlertinscrire(); 
+        }else if ( inscrireData.value1== null && inscrireData.value2==null){
+            $scope.showAlertinscrire();
+        }else {
+            $scope.inscrireModal.hide();
+            $location.path('/tab/dash');
+        }
+               
+    }
+    
+    
+    
 })
 
 .controller('ChatsCtrl', function($scope, Chats) {
