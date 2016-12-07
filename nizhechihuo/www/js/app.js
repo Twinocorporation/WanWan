860bedd8888ab5a208a44038147f9f2b4fd5261f
qiwei <qiweihuang1993@hotmail.fr>
2016-12-07 14:35:30 +0100

    geolocation

--- nizhechihuo/www/js/app.js
+++ nizhechihuo/www/js/app.js
@@ -5,7 +5,7 @@
 // the 2nd parameter is an array of 'requires'
 // 'starter.services' is found in services.js
 // 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services','ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -53,6 +53,13 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
             url:'/homeCuisine',
             templateUrl: 'templates/homeCuisine.html',
             controller: 'HomeCuisineCtrl'
+  })
+  
+   // Page geolocalisation
+  .state('geolocalisation', {
+            url:'/geolocalisation',
+            templateUrl: 'templates/geolocalisation.html',
+            controller: 'GeolocalisationCtrl'
   })
   
   
