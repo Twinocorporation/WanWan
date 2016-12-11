bd246c1bee97fe06532990e0d427053094ac88ff
stylerope <spaceruler@hotmail.com>
2016-12-11 17:03:41 +0100

    11/12/2016

--- nizhechihuo/www/js/app.js
+++ nizhechihuo/www/js/app.js
@@ -184,6 +184,6 @@ angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', '
                     });
 
             // if none of the above states are matched, use this as the fallback
-            $urlRouterProvider.otherwise('/tabCuisine/dashCuisine');
+            $urlRouterProvider.otherwise('/tab/account');
 
         });
